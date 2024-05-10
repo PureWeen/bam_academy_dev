c5c385af6594a9be5650870a10d3de491e8aa2b4
Phillip <threetimberland@gmail.com>
2024-05-10 12:26:53 -0700

    Create lab2_main.py

---
+++ bam23beginners/lesson7/lab2_main.py
@@ -0,0 +1,133 @@
+a = 5 + 2 * 2
+print(a)
+################################# 
+#Lab 2.0 – Evaluating Expressions
+################################# 
+# 
+#Part 1
+#Using the IDE, type in the expressions below and answer questions in comments. Copy and paste the output into the 
+#the result is unexpected, note that.
+#Section 1
+#
+#a = 5 + 2 * 2
+#Expected Output?
+#Did anything unexpected happen?
+#
+#b = 2/3
+#Expected Output?
+#Did anything unexpected happen?
+#
+#c = 2.0 * 1.5
+#Expected Output?
+#Did anything unexpected happen?
+#
+#d = (2 + 3) * 10
+#Expected Output?
+#Did anything unexpected happen?
+# 
+#e = 5.0 // 2
+#Expected Output?
+#Did anything unexpected happen?
+# 
+#f = 5.0 % 2
+#Expected Output?
+#Did anything unexpected happen?
+# 
+########
+#Section 2
+########
+#a = a
+#Did anything unexpected happen?
+#
+#b = 'a'
+#Did anything unexpected happen?
+#
+########
+#Section 3
+########
+#
+#a = 'a + b'
+#Did anything unexpected happen?
+#
+#b = 'a' + 'b'
+#Did anything unexpected happen?
+#
+########
+#Section 4
+########
+#
+#a = 'a' * 'b'
+#Did anything unexpected happen?
+#
+#b = 'a' * 2
+#
+########
+#Part 2
+########
+#Before adding the code in the IDE
+#1.	For each item, predict the data type of the result and enter into the “String/Integer/Float”.
+#2.	Next, predict the value of the result for each item and enter in comment the “Prediction of Result?"
+#	is it of type:  String/Integer/Float
+#
+#a = 10 * 2
+#Prediction of Result? 20
+#Interpreter Result? 20
+#
+#b = .5 * 2
+#Prediction of Result?
+#Interpreter Result?
+#
+#c = 10/2
+#Prediction of Result?
+#Interpreter Result?
+#
+#d = 10%2
+#Prediction of Result?
+#Interpreter Result?
+#
+#e = 2 ** 3
+#Prediction of Result?
+#Interpreter Result?
+#
+#f = (2+5)*3
+#Prediction of Result?
+#Interpreter Result?
+#
+#g = 2 + 5 * 3
+#Prediction of Result?
+#Interpreter Result?
+#
+#h = 'ab' + '12' + '3'
+#Prediction of Result?
+#Interpreter Result?
+#
+#i = x
+#Prediction of Result?
+#Interpreter Result?
+#
+#j = "ab" + "cd"
+#Prediction of Result?
+#Interpreter Result?
+#
+#k = 'abc' * 2
+#Prediction of Result?
+#Interpreter Result?
+#
+#l = '1'*2 + '2' * 3
+#Prediction of Result?
+#Interpreter Result?
+#
+#m = 1 * 2 + '3' * 2
+#Prediction of Result?
+#Interpreter Result?
+#
+#n = 'A' ** 2
+#Interpreter Result?
+#
+#o = 'bc' % 2
+#Interpreter Result?
+#
+#p = 'bc' / 2
+#Interpreter Result?
+
+  
