8a55b8d4f7af7b4b220ee168675e50092fe026fd
Phillip <threetimberland@gmail.com>
2024-05-10 12:25:56 -0700

    Create lab1_main.py

---
+++ bam23beginners/lesson7/lab1_main.py
@@ -0,0 +1,54 @@
+
+
+
+#################################
+#Lab 1.0 – Printing & Variables
+################################# 
+#Part 1 - Printing Practice
+#Practice typing out some statements in the editor part of the IDE, then run them to see the results.
+# 
+#Expression: print("1")
+#Expected Output?
+#Did anything unexpected happen?
+# 
+#Expression: print(1)
+#Expected Output?
+#Did anything unexpected happen?
+# 
+#Expression: print(1 + 2)
+#Expected Output?
+#Did anything unexpected happen?
+#
+#Expression: print("1" + "2")
+#Expected Output?
+#Did anything unexpected happen?
+#
+#Expression: print("this" + " " + "is" + " " + "a" + " " + "sentence" + ".")
+#Expected Output?
+#Did anything unexpected happen?
+#
+#
+#Part 2 - Variables Practice
+#In your Console, Type and run the following
+
+#Expression: animal = "dogs" 
+#Expected Output?
+#Did anything unexpected happen?
+
+#Expression: print(animal + " are really cool.")
+#Expected Output?
+#Did anything unexpected happen?
+
+#Respond to the following
+#1.	What happens?
+#2.	Make the program print out “cats are really cool” instead?
+
+#In your Console, Type and run the following code
+#
+#Expression: print(dogs + " are cool.")
+#Expected Output?
+#Did anything unexpected happen?
+
+#Respond to the following questions
+#1.	What output does this produce?
+#2.	Why does this happen?
